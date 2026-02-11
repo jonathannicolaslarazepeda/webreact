5d6dbd6d11e82c3adcf24bf87e003e47f6565cbe
jonathannicolaslarazepeda <jonathannicolaslarazepeda@alumno.ieselrincon.es>
2026-02-11 18:13:01 +0000

    version 01

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import Header from "../../components/header/Header"
+
+function Home() {
+    return (
+        <>
+            <Header />
+            <h1>HOME</h1>
+        </>
+
+    )
+}
+
+export default Home
